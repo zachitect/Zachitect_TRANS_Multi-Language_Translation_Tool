f2f12fab4d86c20693e9eb7570652ab7f98c45e2
Zach X.G. Zheng <zach@zachitect.com>
2020-01-11 22:50:03 +1100

    Add project files.

---
+++ Mainform.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Zachitect_TRANS
+{
+    partial class Mainform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mainform));
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.TextInput = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.LabelInfo = new System.Windows.Forms.Label();
+            this.TranslateTo = new System.Windows.Forms.ComboBox();
+            this.TranslateFrom = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.TextDisplay = new System.Windows.Forms.RichTextBox();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 506);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(884, 55);
+            this.panel4.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Olive;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OliveDrab;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(884, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Translate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(81)))), ((int)(((byte)(81)))));
+            this.panel2.Controls.Add(this.TextInput);
+            this.panel2.Location = new System.Drawing.Point(0, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(10);
+            this.panel2.Size = new System.Drawing.Size(884, 225);
+            this.panel2.TabIndex = 12;
+            // 
+            // TextInput
+            // 
+            this.TextInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(81)))), ((int)(((byte)(81)))));
+            this.TextInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextInput.ForeColor = System.Drawing.Color.Silver;
+            this.TextInput.Location = new System.Drawing.Point(11, 12);
+            this.TextInput.Margin = new System.Windows.Forms.Padding(2);
+            this.TextInput.Name = "TextInput";
+            this.TextInput.Size = new System.Drawing.Size(861, 201);
+            this.TextInput.TabIndex = 11;
+            this.TextInput.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.LabelInfo);
+            this.panel1.Controls.Add(this.TranslateTo);
+            this.panel1.Controls.Add(this.TranslateFrom);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(884, 50);
+            this.panel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(81)))), ((int)(((byte)(81)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Olive;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OliveDrab;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(436, 15);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 21);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "COPY TO CLIPBOARD";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ErrorImage = global::Zachitect_TRANS.Properties.Resources.Icon_Language;
+            this.pictureBox2.Image = global::Zachitect_TRANS.Properties.Resources.Icon_Language;
+            this.pictureBox2.InitialImage = global::Zachitect_TRANS.Properties.Resources.Icon_Language;
+            this.pictureBox2.Location = new System.Drawing.Point(641, 9);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 32);
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // LabelInfo
+            // 
+            this.LabelInfo.AutoSize = true;
+            this.LabelInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.LabelInfo.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelInfo.ForeColor = System.Drawing.Color.White;
+            this.LabelInfo.Location = new System.Drawing.Point(196, 17);
+            this.LabelInfo.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.LabelInfo.Name = "LabelInfo";
+            this.LabelInfo.Size = new System.Drawing.Size(23, 15);
+            this.LabelInfo.TabIndex = 14;
+            this.LabelInfo.Text = "TO";
+            // 
+            // TranslateTo
+            // 
+            this.TranslateTo.BackColor = System.Drawing.Color.White;
+            this.TranslateTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TranslateTo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TranslateTo.FormattingEnabled = true;
+            this.TranslateTo.Location = new System.Drawing.Point(228, 15);
+            this.TranslateTo.Name = "TranslateTo";
+            this.TranslateTo.Size = new System.Drawing.Size(175, 21);
+            this.TranslateTo.TabIndex = 13;
+            // 
+            // TranslateFrom
+            // 
+            this.TranslateFrom.BackColor = System.Drawing.Color.White;
+            this.TranslateFrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TranslateFrom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TranslateFrom.Location = new System.Drawing.Point(12, 15);
+            this.TranslateFrom.Name = "TranslateFrom";
+            this.TranslateFrom.Size = new System.Drawing.Size(175, 21);
+            this.TranslateFrom.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.ErrorImage = global::Zachitect_TRANS.Properties.Resources.Zachitect_Text_Logo;
+            this.pictureBox1.Image = global::Zachitect_TRANS.Properties.Resources.Zachitect_Text_Logo;
+            this.pictureBox1.InitialImage = global::Zachitect_TRANS.Properties.Resources.Zachitect_Text_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(681, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 32);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(81)))), ((int)(((byte)(81)))));
+            this.panel3.Controls.Add(this.TextDisplay);
+            this.panel3.Location = new System.Drawing.Point(0, 278);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(10);
+            this.panel3.Size = new System.Drawing.Size(884, 225);
+            this.panel3.TabIndex = 13;
+            // 
+            // TextDisplay
+            // 
+            this.TextDisplay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(81)))), ((int)(((byte)(81)))));
+            this.TextDisplay.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextDisplay.ForeColor = System.Drawing.Color.White;
+            this.TextDisplay.Location = new System.Drawing.Point(13, 12);
+            this.TextDisplay.Margin = new System.Windows.Forms.Padding(2);
+            this.TextDisplay.Name = "TextDisplay";
+            this.TextDisplay.Size = new System.Drawing.Size(860, 201);
+            this.TextDisplay.TabIndex = 11;
+            this.TextDisplay.Text = "";
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Mainform";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zachitect.com • Multi-Language Translation Tool";
+            this.panel4.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox TextInput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RichTextBox TextDisplay;
+        private System.Windows.Forms.Label LabelInfo;
+        private System.Windows.Forms.ComboBox TranslateTo;
+        private System.Windows.Forms.ComboBox TranslateFrom;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Mainform.cs
@@ -0,0 +1,208 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Zachitect_TRANS
+{
+    public partial class Mainform : Form
+    {
+        string[] LanguageNames = new string[29]
+        {
+            "Auto-detect Language",
+            "Arabic",
+            "Bulgarian",
+            "Cestina",
+            "Chinese Simplified 简体中文",
+            "Chinese Traditional 繁體中文",
+            "Chinese Classical 文言文",
+            "Chinese Cantonese 广东话",
+            "Danish",
+            "Dutch",
+            "English",
+            "Estonian",
+            "Finnish",
+            "French",
+            "German",
+            "Greek",
+            "Hungarian",
+            "Italian",
+            "Japanese 日本語",
+            "Korean 韓國語",
+            "Polish",
+            "Portuguese",
+            "Romanian",
+            "Russian",
+            "Slovenian",
+            "Spain",
+            "Swedish",
+            "Thai",  
+            "Vietnamese"
+        };
+        string[] LanguageCodes = new string[29]
+        {
+            "auto",
+            "ara",
+            "bul",
+            "cs",
+            "zh",
+            "cht",
+            "wyw",
+            "yue",
+            "dan",
+            "nl",
+            "en",
+            "est",
+            "fin",
+            "fra",
+            "de",
+            "el",
+            "hu",
+            "it",
+            "jp",
+            "kor",
+            "pl",
+            "pt",
+            "rom",
+            "ru",
+            "slo",
+            "spa",
+            "swe",
+            "th",
+            "vie"
+        };
+
+        static String BDTranslate(string q, string from, string to) // 原文
+        {
+            // 源语言
+            // 目标语言
+            // 改成您的APP ID
+            string appId = "20190405000284840";
+            Random rd = new Random();
+            string salt = rd.Next(100000).ToString();
+            // 改成您的密钥
+            string secretKey = "eYK0BQrpLj0tPtPppE32";
+            string sign = EncryptString(appId + q + salt + secretKey);
+            string url = "http://api.fanyi.baidu.com/api/trans/vip/translate?";
+            url += "q=" + HttpUtility.UrlEncode(q);
+            url += "&from=" + from;
+            url += "&to=" + to;
+            url += "&appid=" + appId;
+            url += "&salt=" + salt;
+            url += "&sign=" + sign;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            request.UserAgent = null;
+            request.Timeout = 6000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+        // 计算MD5值
+        public static string EncryptString(string str)
+        {
+            MD5 md5 = MD5.Create();
+            // 将字符串转换成字节数组
+            byte[] byteOld = Encoding.UTF8.GetBytes(str);
+            // 调用加密方法
+            byte[] byteNew = md5.ComputeHash(byteOld);
+            // 将加密结果转换为字符串
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in byteNew)
+            {
+                // 将字节转换成16进制表示的字符串，
+                sb.Append(b.ToString("x2"));
+            }
+            // 返回加密的字符串
+            return sb.ToString();
+        }
+        public static string decode(string text)
+        {
+            byte[] mybyte = System.Convert.FromBase64String(text);
+            string returntext = System.Text.Encoding.UTF8.GetString(mybyte);
+            return returntext;
+        }
+        public Mainform()
+        {
+            InitializeComponent();
+            this.TextInput.Clear();
+            this.TextDisplay.Clear();
+
+            this.TranslateFrom.Items.Clear();
+            this.TranslateTo.Items.Clear();
+
+            this.TranslateFrom.Items.AddRange(LanguageNames);
+            this.TranslateTo.Items.AddRange(LanguageNames);
+
+            this.TranslateFrom.SelectedIndex = 0;
+            this.TranslateTo.SelectedIndex = 4;
+
+            this.TextInput.Text = "Content to translate...";
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TextInput.Text.Length > 0)
+                {
+                    string S = BDTranslate(this.TextInput.Text, LanguageCodes[this.TranslateFrom.SelectedIndex], LanguageCodes[this.TranslateTo.SelectedIndex]);
+                    dynamic res = JsonConvert.DeserializeObject(S);
+                    string[] displayresult = new string[res.trans_result.Count];
+                    for (int i = 0; i < res.trans_result.Count; i++)
+                    {
+                        displayresult[i] = res.trans_result[i].dst;
+                    }
+                    this.TextDisplay.Text = string.Join("\n",displayresult);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                if(TextDisplay.Text.Length > 0)
+                {
+                    Clipboard.SetText(TextDisplay.Text);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("https://www.zachitect.com/translate-tool");
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
